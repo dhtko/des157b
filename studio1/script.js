44af105b9813155e41173480d9cef7c946eb5394
dhtko <dhtko@ucdavis.edu>
2025-04-14 19:36:22 -0700

    studio1

--- studio1/script.js
+++ studio1/script.js
@@ -6,13 +6,14 @@
     const fs = document.querySelector('.fa-expand');
     const troubleshoot = document.querySelector('#troubleshoot');
     const video = document.querySelector('#mainContent');
-
     const lines = document.querySelectorAll('.loopParagraph');
-
+    const loading = document.querySelector('#loadingText');
     let shade = 50;
-
     let lastTime = 0;
 
+
+
+    //troubleshoot text
     troubleshoot.addEventListener('mouseover', function(){
         troubleshoot.className = 'showing';
     });
@@ -20,6 +21,9 @@
         troubleshoot.className = 'hidden';
     });
 
+
+
+    //fullscreen
     fs.addEventListener('click', function(){
         if (!document.fullscreenElement){
             document.documentElement.requestFullscreen();
@@ -28,6 +32,14 @@
             document.exitFullscreen();
         }
     });
+
+/*
+    //loadingText animation
+    video.addEventListener('playing', function(){
+        loading.style.display = 'none';
+    });
+
+
 
     //interactive element: more click, video becomes pale, like we become a zombie
     video.addEventListener('click', function(){
@@ -39,6 +51,9 @@
         video.style.filter = `grayscale(${shade}%)`;
     });
 
+
+
+    //text looping
     video.addEventListener('timeupdate', function(){
         if (video.currentTime < lastTime){
             //when the loop happens the currentTime becomes 0, leaving the lastTime is higher than 0 (for sure)
@@ -48,8 +63,6 @@
         lastTime = video.currentTime;
         //marking the current video play time
     });
-
-
     //show paragraph
     function showParagraph(){
         for (let i = 0; i < lines.length; i++){
@@ -59,7 +72,6 @@
             //delay the appearence on each paragraph after 6s
         }
     }
-
     //looping the paragraph
     function resetParagraph(){
         //hide all paragraphs
@@ -68,8 +80,58 @@
         //restart loop
         showParagraph();
     }
-
     showParagraph();
+*/
+video.addEventListener('click', function(){
+    shade += 10;
+    console.log(`cliker: ${shade}`);
+    if (shade > 200){
+        shade = 50;
+    }
+    video.style.filter = `grayscale(${shade}%)`;
+});
 
 
+//loadingText animation
+video.addEventListener('playing', function(){
+    loading.style.display = 'none';
+
+
+
+    //the looping text starts after the video actually starts
+
+    //interactive element: more click, video becomes pale, like we become a zombie
+
+
+
+
+    //text looping
+    video.addEventListener('timeupdate', function(){
+        if (video.currentTime < lastTime){
+            //when the loop happens the currentTime becomes 0, leaving the lastTime is higher than 0 (for sure)
+            //by catching the difference, we can detect the autoloop
+            resetParagraph();
+        }
+        lastTime = video.currentTime;
+        //marking the current video play time
+    });
+    //show paragraph
+    function showParagraph(){
+        for (let i = 0; i < lines.length; i++){
+            setTimeout(function(){
+                lines[i].className = 'loopParagraph showing';
+            }, 6000 * (i + 1));
+            //delay the appearence on each paragraph after 6s
+        }
+    }
+    //looping the paragraph
+    function resetParagraph(){
+        //hide all paragraphs
+        lines.forEach(p => p.className = 'loopParagraph hidden');
+
+        //restart loop
+        showParagraph();
+    }
+    showParagraph();
+});
 })();
