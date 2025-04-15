6a4c2f4dd6143cd57e75fc3d6b401b094b753d65
dhtko <dhtko@ucdavis.edu>
2025-04-14 18:27:12 -0700

    studio1

--- studio1/script.js
+++ studio1/script.js
@@ -5,6 +5,13 @@
 
     const fs = document.querySelector('.fa-expand');
     const troubleshoot = document.querySelector('#troubleshoot');
+    const video = document.querySelector('#mainContent');
+
+    const lines = document.querySelectorAll('.loopParagraph');
+
+    let shade = 50;
+
+    let lastTime = 0;
 
     troubleshoot.addEventListener('mouseover', function(){
         troubleshoot.className = 'showing';
@@ -21,4 +28,48 @@
             document.exitFullscreen();
         }
     });
+
+    //interactive element: more click, video becomes pale, like we become a zombie
+    video.addEventListener('click', function(){
+        shade += 10;
+        console.log(`cliker: ${shade}`);
+        if (shade > 200){
+            shade = 50;
+        }
+        video.style.filter = `grayscale(${shade}%)`;
+    });
+
+    video.addEventListener('timeupdate', function(){
+        if (video.currentTime < lastTime){
+            //when the loop happens the currentTime becomes 0, leaving the lastTime is higher than 0 (for sure)
+            //by catching the difference, we can detect the autoloop
+            resetParagraph();
+        }
+        lastTime = video.currentTime;
+        //marking the current video play time
+    });
+
+
+    //show paragraph
+    function showParagraph(){
+        for (let i = 0; i < lines.length; i++){
+            setTimeout(function(){
+                lines[i].className = 'loopParagraph showing';
+            }, 6000 * (i + 1));
+            //delay the appearence on each paragraph after 6s
+        }
+    }
+
+    //looping the paragraph
+    function resetParagraph(){
+        //hide all paragraphs
+        lines.forEach(p => p.className = 'loopParagraph hidden');
+
+        //restart loop
+        showParagraph();
+    }
+
+    showParagraph();
+
+
 })();
