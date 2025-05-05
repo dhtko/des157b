fb5b07864bf754ff66c5f1cdebbc3f28640d8748
dhtko <dhtko@ucdavis.edu>
2025-05-04 23:37:59 -0700

    studio3

--- studio3/script.js
+++ studio3/script.js
@@ -2,4 +2,24 @@
     'use strict';
 
     console.log('loaded');
+
+    const granimInstance = new Granim(
+        {
+            element: '#gradient-canvas',
+            name: 'granim-bg',
+            direction: 'left-right',
+            isPausedWhenNotInView: true,
+            states : {
+                "default-state": {
+                    gradients: [
+                        ['#ff9966', '#ff5e62'],
+                        ['#00F260', '#0575E6'],
+                        ['#e1eec3', '#f05053']
+                    ]
+                }
+            }
+        }
+    );
+
+
 })();
