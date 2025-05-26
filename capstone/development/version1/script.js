7adc4d5c5d9f24ec69bab7079f1a90a5e3130205
dhtko <dhtko@ucdavis.edu>
2025-05-26 16:48:35 -0700

    cp ver1

--- capstone/development/version1/script.js
+++ capstone/development/version1/script.js
@@ -17,6 +17,11 @@
     const slideshowAfter = document.querySelector('#after');
     const slideshowImage = document.querySelector('#slideshow');
 
+    const introHeader = document.querySelector('header');
+    const bags = document.querySelectorAll('.garbageBag');
+    const centerText = document.querySelector('#centerText');
+    let i = 0;
+
     scannerCircle.addEventListener('click', function(){
         scannerDetailInfo.className = 'detailInfo showing'
     });
@@ -46,19 +51,29 @@
     });
 
     slideshowBefore.addEventListener('click', function(){
-        console.log('clicked');
         slideshowImage.style.backgroundColor = getRandomRGB();
     });
     slideshowAfter.addEventListener('click', function(){
-        console.log('clicked');
         slideshowImage.style.backgroundColor = getRandomRGB();
     });
-
     function getRandomRGB() {
         const r = Math.floor(Math.random() * 256); // 0 to 255
         const g = Math.floor(Math.random() * 256);
         const b = Math.floor(Math.random() * 256);
         return `rgb(${r}, ${g}, ${b})`;             
     }
+
+    introHeader.addEventListener('click', function(){
+        console.log(i);
+        const bagGroup = bags.length;
+
+        if (i < bagGroup){
+            document.querySelector(`#bag${i+1}`).className = 'garbageBag showing';
+            i++;
+        }
+        else{
+            centerText.className = 'showing';
+        }
+    });
 
 })();
